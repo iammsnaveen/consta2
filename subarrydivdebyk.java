f7d075ca8a356b58da1febc65b95b955cc530117
iammsnaveen <91489717+iammsnaveen@users.noreply.github.com>
2023-02-18 17:30:53 +0530

    Update subarrydivdebyk.java

--- subarrydivdebyk.java
+++ subarrydivdebyk.java
@@ -15,3 +15,20 @@ class Solution {
     return count;
     }
 }
+
+
+
+public static int subarraysDivByB(int[] A, int B) {
+    int[] mods = new int[B]; // Create an array to count the frequency of each mod value
+    mods[0] = 1; // Initialize mods[0] to 1 to handle the case when the entire subarray is divisible by B
+    int sum = 0, count = 0;
+    for (int num : A) {
+        sum = (sum + num) % B; // Calculate the current mod value
+        if (sum < 0) { // Handle negative mod values
+            sum += B;
+        }
+        count += mods[sum]; // Add the frequency of the current mod value to the count
+        mods[sum]++; // Increment the frequency of the current mod value
+    }
+    return count;
+}
